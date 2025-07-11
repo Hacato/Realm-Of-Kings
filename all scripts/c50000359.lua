6f8404a612ed5606e0b3797ed1c68a5a32b1832a
Hacato <148984622+Hacato@users.noreply.github.com>
2025-07-11 19:27:44 -0400

    Add files via upload

---
+++ all scripts/c50000359.lua
@@ -0,0 +1,5 @@
+--Sun Flute Celebration
+local s,id=GetID()
+function s.initial_effect(c)
+	Ritual.AddProcEqualCode(c,9,nil,50000358)
+end
---
+++ all scripts/c50000362.lua
@@ -0,0 +1,5 @@
+--Moon Flute Celebration
+local s,id=GetID()
+function s.initial_effect(c)
+	Ritual.AddProcEqualCode(c,9,nil,50000361)
+end
