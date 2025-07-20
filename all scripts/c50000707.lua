f472629a9c405555be5270d00143db035b349e00
Hacato <148984622+Hacato@users.noreply.github.com>
2025-07-20 13:48:29 -0400

    Add files via upload

--- all scripts/c50000707.lua
+++ all scripts/c50000707.lua
@@ -36,7 +36,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e4)
 	--chain attack
 	local e5=Effect.CreateEffect(c)
-	e5:SetDescription(aux.Stringid(id,2))
+	e5:SetDescription(aux.Stringid(id,0))
 	e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e5:SetCode(EVENT_BATTLE_DESTROYING)
 	e5:SetCountLimit(1)
@@ -44,36 +44,29 @@ function s.initial_effect(c)
 	e5:SetOperation(s.atop)
 	c:RegisterEffect(e5)
 end
+
 function s.spfilter1(c)
 	return c:IsFaceup() and c:IsLevel(8) and c:IsRace(RACE_MACHINE) and c:IsAbleToGraveAsCost()
 end
 function s.spfilter2(c)
 	return c:IsFaceup() and c:IsLevelBelow(4) and c:IsRace(RACE_MACHINE) and c:IsType(TYPE_FLIP) and c:IsAbleToGraveAsCost()
 end
-function s.rescon(sg,e,tp,mg)
-	return aux.ChkfMMZ(1)(sg,e,tp,mg) and sg:IsExists(s.chk,1,nil,sg)
-end
-function s.chk(c,sg)
-	return s.spfilter1(c) and sg:IsExists(s.spfilter2,1,c)
-end
 function s.spcon(e,c)
 	if c==nil then return true end
 	local tp=e:GetHandlerPlayer()
 	local g1=Duel.GetMatchingGroup(s.spfilter1,tp,LOCATION_MZONE,0,nil)
 	local g2=Duel.GetMatchingGroup(s.spfilter2,tp,LOCATION_MZONE,0,nil)
-	local g=g1:Clone()
-	g:Merge(g2)
-	return Duel.GetLocationCount(tp,LOCATION_MZONE)>-2 and #g1>0 and #g2>0 and aux.SelectUnselectGroup(g,e,tp,2,2,s.rescon,0)
+	return Duel.GetLocationCount(tp,LOCATION_MZONE)>-2 and #g1>0 and #g2>0
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,c)
-	local c=e:GetHandler()
 	local g1=Duel.GetMatchingGroup(s.spfilter1,tp,LOCATION_MZONE,0,nil)
 	local g2=Duel.GetMatchingGroup(s.spfilter2,tp,LOCATION_MZONE,0,nil)
-	g1:Merge(g2)
-	local g=aux.SelectUnselectGroup(g1,e,tp,2,2,s.rescon,1,tp,HINTMSG_TOGRAVE)
-	if #g>0 then
-		g:KeepAlive()
-		e:SetLabelObject(g)
+	local sg1=g1:Select(tp,1,1,nil)
+	local sg2=g2:Select(tp,1,1,nil)
+	sg1:Merge(sg2)
+	if #sg1==2 then
+		sg1:KeepAlive()
+		e:SetLabelObject(sg1)
 		return true
 	end
 	return false
@@ -82,17 +75,29 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
 	local g=e:GetLabelObject()
 	if not g then return end
 	Duel.SendtoGrave(g,REASON_COST)
+	g:DeleteGroup()
 end
-function s.efilter(e,te)
-	return not (te:IsActiveType(TYPE_FIELD) and te:IsActiveType(TYPE_SPELL))
-end
-function s.filter(c)
-	return c:IsFacedown() and c:GetSequence()==5
+
+--Check for face-down cards in Field Zones (sequence 5 for both players)
+function s.fieldfilter(c)
+	return c:IsFacedown() and c:IsLocation(LOCATION_FZONE)
 end
 function s.immcon(e)
-	local tp=e:GetHandlerPlayer()
-	return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_SZONE,LOCATION_SZONE,1,nil)
+	return Duel.IsExistingMatchingCard(s.fieldfilter,0,LOCATION_FZONE,LOCATION_FZONE,1,nil)
 end
+
+--Fixed immunity filter: unaffected by opponent's effects except Field Spells
+function s.efilter(e,te)
+	local c=e:GetHandler()
+	local tc=te:GetOwner()
+	--Only affects opponent's card effects
+	if tc:GetControler()==c:GetControler() then return false end
+	--Exception: opponent's Field Spells still affect this card
+	if te:IsActiveType(TYPE_FIELD) and te:IsActiveType(TYPE_SPELL) then return false end
+	--Block all other opponent effects
+	return true
+end
+
 function s.atcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	return Duel.GetAttacker()==c and aux.bdocon(e,tp,eg,ep,ev,re,r,rp) and c:CanChainAttack()
