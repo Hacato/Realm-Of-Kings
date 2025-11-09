01ed67cf210f9435ac7c2d1c1fcd49c02d6a43e2
Hacato <148984622+Hacato@users.noreply.github.com>
2025-11-09 18:24:25 -0500

    Add files via upload

--- all scripts/c800000084.lua
+++ all scripts/c800000084.lua
@@ -2,38 +2,59 @@
 --Scripted by Assistant
 function c800000084.initial_effect(c)
   --(1) Ritual Summon with ATK boost
-  Ritual.AddProcGreater({
-    handler=c,
-    filter=c800000084.ritualfilter,
-    location=LOCATION_HAND|LOCATION_EXTRA,
-    stage2=c800000084.stage2
-  })
+  local e1=Effect.CreateEffect(c)
+  e1:SetDescription(aux.Stringid(800000084,0))
+  e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+  e1:SetType(EFFECT_TYPE_ACTIVATE)
+  e1:SetCode(EVENT_FREE_CHAIN)
+  e1:SetTarget(c800000084.target)
+  e1:SetOperation(c800000084.activate)
+  c:RegisterEffect(e1)
 end
-function c800000084.ritualfilter(c)
-  return c:IsSetCard(0x991) and c:IsRitualMonster() and (c:IsFaceup() or c:IsLocation(LOCATION_HAND))
+--(1) Ritual Summon
+function c800000084.filter(c,e,tp)
+  return c:IsSetCard(0x991) and c:IsType(TYPE_RITUAL) and c:IsType(TYPE_MONSTER)
+    and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_RITUAL,tp,false,true)
 end
-function c800000084.stage2(mat,e,tp,eg,ep,ev,re,r,rp,tc)
-  -- ATK boost effect
-  local ct=Duel.GetMatchingGroupCount(Card.IsSetCard,tp,LOCATION_MZONE,0,nil,0x991)
-  if ct>0 then
-    local e1=Effect.CreateEffect(e:GetHandler())
-    e1:SetType(EFFECT_TYPE_SINGLE)
-    e1:SetCode(EFFECT_UPDATE_ATTACK)
-    e1:SetValue(ct*500)
-    e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END+RESET_OPPO_TURN,1)
-    tc:RegisterEffect(e1)
+function c800000084.target(e,tp,eg,ep,ev,re,r,rp,chk)
+  if chk==0 then
+    local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+    if ft<=0 then return false end
+    return Duel.IsExistingMatchingCard(c800000084.filter,tp,LOCATION_HAND,0,1,nil,e,tp)
+      or Duel.IsExistingMatchingCard(aux.AND(c800000084.filter,Card.IsFaceup),tp,LOCATION_EXTRA,0,1,nil,e,tp)
   end
-  
-  -- LP loss at End Phase
-  if Duel.GetFlagEffect(tp,800000084)==0 then
-    Duel.RegisterFlagEffect(tp,800000084,RESET_PHASE+PHASE_END,0,1)
-    local e2=Effect.CreateEffect(e:GetHandler())
-    e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-    e2:SetCode(EVENT_PHASE+PHASE_END)
-    e2:SetCountLimit(1)
-    e2:SetReset(RESET_PHASE+PHASE_END)
-    e2:SetOperation(c800000084.loseop)
-    Duel.RegisterEffect(e2,tp)
+  Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND|LOCATION_EXTRA)
+end
+function c800000084.activate(e,tp,eg,ep,ev,re,r,rp)
+  if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+  Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+  local g=Duel.GetMatchingGroup(c800000084.filter,tp,LOCATION_HAND,0,nil,e,tp)
+  local eg=Duel.GetMatchingGroup(aux.AND(c800000084.filter,Card.IsFaceup),tp,LOCATION_EXTRA,0,nil,e,tp)
+  g:Merge(eg)
+  if #g>0 then
+    local tc=g:Select(tp,1,1,nil):GetFirst()
+    if tc and Duel.SpecialSummon(tc,SUMMON_TYPE_RITUAL,tp,tp,false,true,POS_FACEUP)>0 then
+      tc:CompleteProcedure()
+      -- ATK boost effect
+      local ct=Duel.GetMatchingGroupCount(Card.IsSetCard,tp,LOCATION_MZONE,0,nil,0x991)
+      if ct>0 then
+        local e1=Effect.CreateEffect(e:GetHandler())
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetCode(EFFECT_UPDATE_ATTACK)
+        e1:SetValue(ct*500)
+        e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END+RESET_OPPO_TURN,1)
+        tc:RegisterEffect(e1)
+      end
+      
+      -- LP loss at End Phase (per use)
+      local e2=Effect.CreateEffect(e:GetHandler())
+      e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+      e2:SetCode(EVENT_PHASE+PHASE_END)
+      e2:SetCountLimit(1)
+      e2:SetReset(RESET_PHASE+PHASE_END)
+      e2:SetOperation(c800000084.loseop)
+      Duel.RegisterEffect(e2,tp)
+    end
   end
 end
 function c800000084.loseop(e,tp,eg,ep,ev,re,r,rp)
--- all scripts/c99910010.lua
+++ all scripts/c99910010.lua
@@ -1,107 +1,79 @@
 --YuYuYu Ritual Spell
---Scripted by Raivost, updated by Copilot as per Hacato's request
+--Scripted by Raivost
 function c99910010.initial_effect(c)
-  --(1) Ritual Summon
+  --(1) Ritual Summon and Equip
   local e1=Effect.CreateEffect(c)
   e1:SetDescription(aux.Stringid(99910010,0))
-  e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+  e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_EQUIP)
   e1:SetType(EFFECT_TYPE_ACTIVATE)
   e1:SetCode(EVENT_FREE_CHAIN)
-  e1:SetTarget(c99910010.target)
-  e1:SetOperation(c99910010.activate)
+  e1:SetTarget(c99910010.sptg)
+  e1:SetOperation(c99910010.spop)
   c:RegisterEffect(e1)
 end
-function c99910010.filter(c,e,tp)
+--(1) Ritual Summon and Equip
+function c99910010.spfilter(c,e,tp)
   return c:IsSetCard(0x991) and bit.band(c:GetType(),0x81)==0x81 and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_RITUAL,tp,true,false)
 end
-function c99910010.target(e,tp,eg,ep,ev,re,r,rp,chk)
-  if chk==0 then
-    if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return false end
-    local g=Duel.GetMatchingGroup(c99910010.filter,tp,LOCATION_HAND,0,nil,e,tp)
-    if #g==0 then return false end
-    local mg=Duel.GetRitualMaterial(tp)
-    return g:IsExists(c99910010.ritualcheck,1,nil,mg)
-  end
+function c99910010.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+  if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+  and Duel.IsExistingMatchingCard(c99910010.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp) end
   Duel.Hint(HINT_OPSELECTED,1-tp,e:GetDescription())
   Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
-end
-function c99910010.ritualcheck(c,mg)
-  local testmg=mg:Clone()
-  testmg:RemoveCard(c)
-  return testmg:CheckWithSumGreater(Card.GetRitualLevel,c:GetLevel(),c)
 end
 function c99910010.eqfilter(c)
   return c:IsSetCard(0x991) and c:IsRace(RACE_FAIRY) and not c:IsForbidden()
 end
-function c99910010.activate(e,tp,eg,ep,ev,re,r,rp)
+function c99910010.spop(e,tp,eg,ep,ev,re,r,rp)
   if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
   Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-  local g=Duel.SelectMatchingCard(tp,c99910010.filter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
-  local tc=g:GetFirst()
-  if not tc then return end
-  
-  -- Ritual Material selection
-  local mg=Duel.GetRitualMaterial(tp)
-  mg:RemoveCard(tc)
-  if tc:IsLocation(LOCATION_HAND) then
-    mg:RemoveCard(tc)
-  end
-  
-  Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
-  local mat=mg:SelectWithSumGreater(tp,Card.GetRitualLevel,tc:GetLevel(),tc)
-  if not mat or #mat==0 then return end
-  
-  tc:SetMaterial(mat)
-  Duel.ReleaseRitualMaterial(mat)
-  Duel.BreakEffect()
-  if Duel.SpecialSummon(tc,SUMMON_TYPE_RITUAL,tp,tp,true,false,POS_FACEUP)~=0 then
+  local tg=Duel.SelectMatchingCard(tp,c99910010.spfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+  local tc=tg:GetFirst()
+  if tc and Duel.SpecialSummon(tc,SUMMON_TYPE_RITUAL,tp,tp,true,false,POS_FACEUP)~=0 then
     tc:CompleteProcedure()
-    
-    -- Equip effect
-    local sea_of_trees=Duel.IsEnvironment(99910070)
-    local b1=sea_of_trees and Duel.IsExistingMatchingCard(c99910010.eqfilter,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE,0,1,nil)
-    local b2=Duel.IsExistingMatchingCard(c99910010.eqfilter,tp,LOCATION_HAND,0,1,nil)
-    
-    if (b1 or b2) and Duel.GetLocationCount(tp,LOCATION_SZONE)>0 and Duel.SelectYesNo(tp,aux.Stringid(99910010,1)) then
-      Duel.BreakEffect()
-      Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
-      local eq
-      if b1 then
-        eq=Duel.SelectMatchingCard(tp,c99910010.eqfilter,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil):GetFirst()
-      else
-        eq=Duel.SelectMatchingCard(tp,c99910010.eqfilter,tp,LOCATION_HAND,0,1,1,nil):GetFirst()
-      end
-      
-      if eq then
-        Duel.Equip(tp,eq,tc)
-        -- Grant equip effect
-        local e1=Effect.CreateEffect(e:GetHandler())
-        e1:SetType(EFFECT_TYPE_SINGLE)
-        e1:SetCode(EFFECT_EQUIP_LIMIT)
-        e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-        e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-        e1:SetValue(c99910010.eqlimit)
-        e1:SetLabelObject(tc)
-        eq:RegisterEffect(e1)
+    if Duel.GetLocationCount(tp,LOCATION_SZONE)>0 then
+      local b1=Duel.IsEnvironment(99910070) and Duel.IsExistingMatchingCard(c99910010.eqfilter,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE,0,1,nil)
+      local b2=Duel.IsExistingMatchingCard(c99910010.eqfilter,tp,LOCATION_HAND,0,1,nil)
+      if b1 or b2 then
+        if Duel.SelectYesNo(tp,aux.Stringid(99910010,1)) then
+          Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
+          local g=nil
+          if b1 then
+            g=Duel.SelectMatchingCard(tp,c99910010.eqfilter,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
+          else
+            g=Duel.SelectMatchingCard(tp,c99910010.eqfilter,tp,LOCATION_HAND,0,1,1,nil)
+          end
+          local ec=g:GetFirst()
+          if ec then
+            Duel.BreakEffect()
+            Duel.Equip(tp,ec,tc)
+            --Equip limit
+            local e1=Effect.CreateEffect(e:GetHandler())
+            e1:SetType(EFFECT_TYPE_SINGLE)
+            e1:SetCode(EFFECT_EQUIP_LIMIT)
+            e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+            e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+            e1:SetValue(c99910010.eqlimit)
+            e1:SetLabelObject(tc)
+            ec:RegisterEffect(e1)
+          end
+        end
       end
     end
-    
-    -- LP loss at End Phase
-    if Duel.GetFlagEffect(tp,99910010)==0 then
-      Duel.RegisterFlagEffect(tp,99910010,RESET_PHASE+PHASE_END,0,1)
-      local e2=Effect.CreateEffect(e:GetHandler())
-      e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-      e2:SetCode(EVENT_PHASE+PHASE_END)
-      e2:SetCountLimit(1)
-      e2:SetReset(RESET_PHASE+PHASE_END)
-      e2:SetOperation(c99910010.loseop)
-      Duel.RegisterEffect(e2,tp)
-    end
+    --(1.1) Lose LP
+    local e2=Effect.CreateEffect(e:GetHandler())
+    e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+    e2:SetCode(EVENT_PHASE+PHASE_END)
+    e2:SetCountLimit(1)
+    e2:SetReset(RESET_PHASE+PHASE_END)
+    e2:SetOperation(c99910010.loseop)
+    Duel.RegisterEffect(e2,tp)
   end
 end
 function c99910010.eqlimit(e,c)
   return c==e:GetLabelObject()
 end
+--(1.1) Lose LP
 function c99910010.loseop(e,tp,eg,ep,ev,re,r,rp)
   Duel.Hint(HINT_CARD,0,99910010)
   Duel.SetLP(tp,Duel.GetLP(tp)-1000)
