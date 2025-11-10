39f6ec2600062d0310a42d5f8821af611cf9d8ef
Hacato <148984622+Hacato@users.noreply.github.com>
2025-11-10 17:17:49 -0500

    Add files via upload

--- all scripts/c800000090.lua
+++ all scripts/c800000090.lua
@@ -1,7 +1,7 @@
 --YuYuYu Blooming Hero
 local s,id=GetID()
 function s.initial_effect(c)
-	--Add 1 "YuYuYu" Pendulum Monster to Extra Deck face-up, then Special Summon 1 "YuYuYu" Link Monster
+	--Add 1 "YuYuYu" Pendulum Monster from your hand or field to the Extra Deck face-up, then Special Summon 1 "YuYuYu" Link Monster from your Extra Deck, and if you do, it gains 500 ATK, but it is destroyed at the end of the turn
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
@@ -12,22 +12,18 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 s.listed_series={0x991}
-
 function s.penfilter(c)
 	return c:IsSetCard(0x991) and c:IsType(TYPE_PENDULUM) and not c:IsForbidden()
 end
-
 function s.spfilter(c,e,tp)
-	return c:IsSetCard(0x991) and c:IsType(TYPE_LINK) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return c:IsSetCard(0x991) and c:IsType(TYPE_LINK) and c:IsCanBeSpecialSummoned(e,0,tp,true,false)
 end
-
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.penfilter,tp,LOCATION_HAND+LOCATION_MZONE,0,1,nil)
 		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE,0,1,nil,e,tp) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE)
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end
-
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
 	local g=Duel.SelectMatchingCard(tp,s.penfilter,tp,LOCATION_HAND+LOCATION_MZONE,0,1,1,nil)
@@ -35,8 +31,8 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	if tc and Duel.SendtoExtraP(tc,nil,REASON_EFFECT)~=0 and tc:IsLocation(LOCATION_EXTRA) then
 		if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local sg=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.spfilter),tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil,e,tp)
-		if #sg>0 and Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)~=0 then
+		local sg=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp)
+		if #sg>0 and Duel.SpecialSummon(sg,0,tp,tp,true,false,POS_FACEUP)~=0 then
 			local sc=sg:GetFirst()
 			--Gain 500 ATK
 			local e1=Effect.CreateEffect(e:GetHandler())
@@ -59,12 +55,10 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 		end
 	end
 end
-
 function s.descon(e,tp,eg,ep,ev,re,r,rp)
 	local tc=e:GetLabelObject()
 	return tc and tc:IsLocation(LOCATION_MZONE)
 end
-
 function s.desop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=e:GetLabelObject()
 	Duel.Destroy(tc,REASON_EFFECT)
