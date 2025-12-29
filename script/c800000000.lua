b1cd4d8f4171ff4431e262057a0b6214db601f3c
Hacato <148984622+Hacato@users.noreply.github.com>
2025-12-29 02:13:21 -0500

    Add files via upload

--- script/c800000000.lua
+++ script/c800000000.lua
@@ -1,30 +1,61 @@
---Lance
---Search your deck for up to 3 Dragon type Monster, reveal them, and put them into your hand. Then, shuffle your deck.
+--Dragon Master
+--Discard 1 card; add up to 2 Level 3 or Level 5 Dragon monsters with different names from your Deck to your hand. You can only activate 1 “Dragon Master” per turn.
 
 local s,id=GetID()
 function s.initial_effect(c)
-	--Search Dragons
+	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
 	c:RegisterEffect(e1)
 end
 
+--Cost: Discard 1 card
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil) end
+	Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD,nil)
+end
+
+--Filter: Only Level 3 or Level 5 Dragons
 function s.filter(c)
-	return c:IsRace(RACE_DRAGON) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
+	return c:IsRace(RACE_DRAGON) and c:IsType(TYPE_MONSTER)
+		and (c:IsLevel(3) or c:IsLevel(5))
+		and c:IsAbleToHand()
+end
+
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then 
+		return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil)
+	end
+	-- We *might* add up to 2, so just say 2 here
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,2,tp,LOCATION_DECK)
 end
 
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_DECK,0,nil)
-	if #g>0 then
+	if #g==0 then return end
+
+	-- First mandatory pick
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local sg=g:Select(tp,1,1,nil)
+	if #sg==0 then return end
+	local tc=sg:GetFirst()
+
+	-- Remove all copies of that name from the pool for the second pick
+	g:Remove(Card.IsCode,nil,tc:GetCode())
+
+	-- Optional second pick (must have different name)
+	if #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-		local sg=g:Select(tp,1,3,nil)
-		if #sg>0 then
-			Duel.SendtoHand(sg,nil,REASON_EFFECT)
-			Duel.ConfirmCards(1-tp,sg)
-		end
+		local sg2=g:Select(tp,1,1,nil)
+		sg:Merge(sg2)
 	end
-	Duel.ShuffleDeck(tp)
+
+	Duel.SendtoHand(sg,nil,REASON_EFFECT)
+	Duel.ConfirmCards(1-tp,sg)
 end
