313214dd302b1912a89ac59f1e37ccc3516c314b
Hacato <148984622+Hacato@users.noreply.github.com>
2026-03-15 02:41:41 -0400

    Add files via upload

--- script/c800000197.lua
+++ script/c800000197.lua
@@ -9,7 +9,7 @@ function s.initial_effect(c)
 	e1:SetCode(EFFECT_SPSUMMON_PROC)
 	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
 	e1:SetRange(LOCATION_HAND)
-	e1:SetCountLimit(1,id)
+	e1:SetCountLimit(1,id) -- soft once per turn per copy
 	e1:SetCondition(s.hspcon)
 	c:RegisterEffect(e1)
 
@@ -20,7 +20,7 @@ function s.initial_effect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e2:SetProperty(EFFECT_FLAG_DELAY)
 	e2:SetCode(EVENT_SUMMON_SUCCESS)
-	e2:SetCountLimit(1,id+1)
+	e2:SetCountLimit(1) -- soft once per turn per copy
 	e2:SetTarget(s.target)
 	e2:SetOperation(s.operation)
 	c:RegisterEffect(e2)
@@ -30,7 +30,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e3)
 end
 
---Special Summon condition
+--Special Summon condition from hand
 function s.hspcon(e,c)
 	if c==nil then return true end
 	local tp=c:GetControler()
@@ -39,7 +39,7 @@ function s.hspcon(e,c)
 		and (g:GetCount()==0 or (g:FilterCount(Card.IsRace,nil,RACE_THUNDER)==g:GetCount() and not g:IsExists(Card.IsFacedown,1,nil)))
 end
 
---Level 4 LIGHT Thunder ≤1600 ATK
+--Level 4 LIGHT Thunder ≤1600 ATK (except itself)
 function s.spfilter(c,e,tp)
 	return c:IsRace(RACE_THUNDER)
 		and c:IsAttribute(ATTRIBUTE_LIGHT)
@@ -49,7 +49,7 @@ function s.spfilter(c,e,tp)
 		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 
---Watt search
+--Optional Watt search (except itself)
 function s.thfilter(c)
 	return c:IsSetCard(0xe)
 		and not c:IsCode(id)
@@ -57,27 +57,30 @@ function s.thfilter(c)
 		and c:IsAbleToHand()
 end
 
+--Targeting check
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
 		return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 			and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil,e,tp)
-			and Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil)
 	end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
 end
 
+--Operation: Special Summon + optional add-to-hand
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 
-	--Special Summon
+	--Special Summon Level 4 LIGHT Thunder
 	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil,e,tp)
 	if #g>0 and Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)>0 then
-		--Add Watt monster
-		local hg=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
-		if #hg>0 then
-			Duel.SendtoHand(hg,nil,REASON_EFFECT)
-			Duel.ConfirmCards(1-tp,hg)
+		--Optional: Add a Watt monster to hand
+		if Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil)
+			and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+			local hg=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
+			if #hg>0 then
+				Duel.SendtoHand(hg,nil,REASON_EFFECT)
+				Duel.ConfirmCards(1-tp,hg)
+			end
 		end
 	end
 end
